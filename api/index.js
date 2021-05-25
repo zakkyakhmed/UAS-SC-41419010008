925861a3b71dfe43c814496269f38b148b24b76b
zendi014 <48636084+zendi014@users.noreply.github.com>
2021-05-25 13:07:15 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -20,41 +20,6 @@ bot.onText(/\/start/, (msg) => {
     );   
 });
 
-// input requires i and r
-state = 0;
-bot.onText(/\/predict/, (msg) => { 
-    bot.sendMessage(
-        msg.chat.id,
-        `masukan nilai i|v contohnya 9|9`
-    );   
-    state = 1;
-});
-
-bot.on('message', (msg) => {
-    if(state == 1){
-        s = msg.text.split("|");
-        i = s[0]
-        v = s[1]
-        model.predict(
-            [
-                parseFloat(s[0]), // string to float
-                parseFloat(s[1])
-            ]
-        ).then((jres)=>{
-            bot.sendMessage(
-                msg.chat.id,
-                `nilai v yang diprediksi adalah ${jres[0]} volt`
-            ); 
-            bot.sendMessage(
-                msg.chat.id,
-                `nilai p yang diprediksi adalah ${jres[1]} watt`
-            ); 
-        })
-    }else{
-        state = 0 
-    }
-})
-
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
     model.predict(
