ebc8a622a17b8040c225fde24339521eb4ae65f2
zendi014 <48636084+zendi014@users.noreply.github.com>
2021-05-28 18:49:32 +0700

    Create cls_model.js

---
+++ api/sdk/cls_model.js
@@ -0,0 +1,53 @@
+const tf = require('@tensorflow/tfjs-node');
+const argmax = require('compute-argmax')
+
+function normalized(data){ // i & r
+    i = (data[0] - 12.585) / 6.813882
+    r = (data[1] - 51.4795) / 29.151289
+    v = (data[2] - 650.4795) / 552.6351
+    p = (data[3] - 10620.56) / 12152.78
+    return [i, r, v, p]
+}
+
+function amax(arr_res){
+    cls_data = []
+    label = "Normal"
+    for(i=0; i< arr_res.length;i++){
+        cls_data[i] = arr_res[i]
+    }
+    if(argmax(cls_data) == 1){
+        label = "Over Voltage"
+    }
+    if(argmax(cls_data) == 2){
+        label = "Drop Voltage"
+    }
+    return label;
+}
+
+async function classify(data){
+    let in_dim = 4;
+    
+    data = normalized(data);
+    shape = [1, in_dim];
+
+    tf_data = tf.tensor2d(data, shape);
+
+    try{
+        // path load in public access => github
+        const path = 'https://raw.githubusercontent.com/zendi014/bot-jst/main/public/cls_model/model.json';
+        const model = await tf.loadGraphModel(path);
+        
+        predict = model.predict(
+                tf_data
+        );
+        result = predict.dataSync();
+        return amax(result);
+        
+    }catch(e){
+      console.log(e);
+    }
+}
+
+module.exports = {
+    classify: classify 
+}
