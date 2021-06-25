38dbb2b0e45c2b2b8232ee494e611370a81cc016
zendi014 <48636084+zendi014@users.noreply.github.com>
2021-06-25 16:49:34 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 let i = 0;
 let r = 0;
-router.get('set/:i/:r/', function(req, res, next) {
+router.get('/set/:i/:r/', function(req, res, next) {
   i = req.params.i;
   r = req.params.r;
   res.redirect(`/api/classify/${i}/${r}`)
