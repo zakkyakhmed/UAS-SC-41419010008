35146094ab916b0451b3468b66a0bd4e0b84fe3f
zendi014 <48636084+zendi014@users.noreply.github.com>
2021-06-25 17:07:21 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ let r = 0;
 router.get('/set/:i/:r/', function(req, res, next) {
   i = req.params.i;
   r = req.params.r;
-  res.redirect(`/api/classify/${i}/${r}`)
+  res.json({i:i, r:r})
 });
 
 router.get('/:sel', function(req, res, next) {
